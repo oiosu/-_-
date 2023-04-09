5fcbc0ce3bba428fe243efdf300dacf9a38cd892
best.fe.su <kdlatnrud123@naver.com>
2023-04-09 20:44:09 +0900

    37 자릿수 구하기

---
+++ 프로그래머스_Level 0/37_자릿수 구하기.py
@@ -0,0 +1,15 @@
+# 문제 설명
+# 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+
+# 제한사항
+
+#     0 ≤ n ≤ 1,000,000
+
+# 입출력 예
+# n 	result
+# 1234 	10
+# 930211 	16
+
+def solution(n):
+    a = list(map(int, str(n)))
+    return sum(a)
