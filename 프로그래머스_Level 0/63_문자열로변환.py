c7767ae63aa09bc67a4a67202ab4f19a5fcc5657
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-04-26 09:36:17 +0900

    Create 63_문자열로변환.py

---
+++ 프로그래머스_Level 0/63_문자열로변환.py
@@ -0,0 +1,10 @@
+# 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+# str()으로 숫자를 문자로 변환
+# format()으로 숫자를 문자로 변환
+# f-string 으로 숫자를 문자로 변환
+
+def solution(n):
+    answer = ''
+    answer = f'{n}'
+    return answer
