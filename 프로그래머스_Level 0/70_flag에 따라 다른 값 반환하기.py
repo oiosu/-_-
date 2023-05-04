4dc40183e95d79cf11bf0483a5ad33528998eba5
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-05-04 17:20:56 +0900

    Create 70_flag에 따라 다른 값 반환하기.py

---
+++ 프로그래머스_Level 0/70_flag에 따라 다른 값 반환하기.py
@@ -0,0 +1,19 @@
+# flag에 따라 다른 값 반환하기
+# 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 
+# false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+# 조건문 
+
+#case1
+def solution(a, b, flag):
+  answer = 0
+  if flag == True:
+    answer = a + b
+  else:
+    answer = a - b
+  return answer
+
+# case2
+def solution(a, b, flag):
+    if flag = return a + b:
+    return a - b
