1d58c334e14757bd4da1b0fde86295ef58b3c3ec
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-05-04 17:03:13 +0900

    Create 69_정수부분.py

---
+++ 프로그래머스_Level 0/69_정수부분.py
@@ -0,0 +1,15 @@
+# 정수 부분 
+# 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+# flo	    result
+# 1.42	  1
+# 69.32	  69
+
+# float을 int로 변경하는 방법
+# 1.  int()를 이용하여 변환
+# 2. ceil(), floor(), round()를 이용하여 변환
+
+def solution(flo):
+    answer = 0
+    answer = int(flo)
+    return answer
